819c708d4d270c96fbfdc04123886bef72573234
Pradyumna <pradyumna.deshmukh21@gmail.com>
2024-04-23 12:16:15 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println("This is a program to test a jenkins from github");
+        System.out.println("Hello");
+    }
+}
